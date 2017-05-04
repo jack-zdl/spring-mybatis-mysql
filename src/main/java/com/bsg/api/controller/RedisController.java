5513e14a29eb711a75538095e2587b1642c9dd5f
jack-zdl <zhangdelei@bsgchina.com>
2017-05-04 20:21:14 +0800

    redis 新增数据成功

--- src/main/java/com/bsg/api/controller/RedisController.java
+++ src/main/java/com/bsg/api/controller/RedisController.java
@@ -1,9 +1,12 @@
 package com.bsg.api.controller;
 
+import com.bsg.api.exception.APIException;
+import com.bsg.api.service.RedisService;
 import com.bsg.api.util.RespJson;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import java.util.Map;
 
@@ -11,25 +14,33 @@ import java.util.Map;
  * Created by zhang on 2017/5/4.
  */
 @Controller
-@RequestMapping("V1.0/redis")
+@RequestMapping("v1.0/redis")
 public class RedisController {
+    @Resource
+    private RedisService redisService;
+
     @RequestMapping(method = RequestMethod.GET)
     @ResponseBody
-    public RespJson list(HttpServletRequest request, @RequestParam Map<String, Object> param) {
+    public RespJson list(HttpServletRequest request, @RequestBody Map<String, Object> param) {
         RespJson respJson = null;
         return respJson;
     }
 
     @RequestMapping(method = RequestMethod.POST)
     @ResponseBody
-    public RespJson save(HttpServletRequest request, @RequestParam Map<String, Object> param) {
+    public RespJson save(HttpServletRequest request, @RequestBody Map<String, Object> param) throws APIException {
         RespJson respJson = null;
+        try {
+            respJson = redisService.save(request, param);
+        } catch (Exception e) {
+            throw new APIException();
+        }
         return respJson;
     }
 
     @RequestMapping(method = RequestMethod.PUT)
     @ResponseBody
-    public RespJson update(HttpServletRequest request, @RequestParam Map<String, Object> param) {
+    public RespJson update(HttpServletRequest request, @RequestBody Map<String, Object> param) {
         RespJson respJson = null;
         return respJson;
     }
--- src/main/java/com/bsg/api/service/RedisService.java
+++ src/main/java/com/bsg/api/service/RedisService.java
@@ -2,6 +2,11 @@ package com.bsg.api.service;
 
 import com.bsg.api.exception.APIException;
 import com.bsg.api.util.RespJson;
+import com.bsg.api.util.RespJsonFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
 
 import javax.servlet.http.HttpServletRequest;
@@ -12,30 +17,62 @@ import java.util.Map;
  */
 @Service()
 public class RedisService extends BaseService {
-    
+
+    ApplicationContext ctx = new ClassPathXmlApplicationContext("classpath:config/spring-redis.xml");
+    RedisTemplate redisTemplate = (RedisTemplate) ctx.getBean("redisTemplate");
+    StringRedisTemplate stringRedisTemplate = (StringRedisTemplate) ctx.getBean("stringRedisTemplate");
 
     @Override
     public RespJson list(HttpServletRequest request, Map<String, Object> param) throws APIException {
-        return null;
+        RespJson respJson = null;
+        return respJson;
     }
 
     @Override
     public RespJson get(HttpServletRequest request, Map<String, Object> param) throws APIException {
-        return null;
+        RespJson respJson = null;
+        try {
+
+        } catch (Exception e) {
+            throw new APIException();
+        }
+        return respJson;
     }
 
     @Override
     public RespJson save(HttpServletRequest request, Map<String, Object> param) throws APIException {
-        return null;
+        RespJson respJson = null;
+        try {
+            String key = "redisKey";
+            String value = "redisValue";
+            redisTemplate.opsForValue().set(key, value);
+            respJson = RespJsonFactory.buildSuccess("redis数据存储成功！");
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new APIException();
+        }
+        return respJson;
     }
 
     @Override
     public RespJson update(HttpServletRequest request, Map<String, Object> param) throws APIException {
-        return null;
+        RespJson respJson = null;
+        try {
+
+        } catch (Exception e) {
+            throw new APIException();
+        }
+        return respJson;
     }
 
     @Override
     public RespJson remove(HttpServletRequest request, String id) throws APIException {
-        return null;
+        RespJson respJson = null;
+        try {
+
+        } catch (Exception e) {
+            throw new APIException();
+        }
+        return respJson;
     }
 }
